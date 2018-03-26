3ed28b0234a88d803ccd7aa35e5b04166201e9b4
Xavier Clerc <xclerc@janestreet.com>
2018-03-26 13:04:47 +0100

    Tweak deriving_inline tests.

--- test/type-conv/test.ml
+++ test/type-conv/test.ml
@@ -1,9 +1,9 @@
 open Base
 
 type t = int [@@deriving_inline sexp, compare]
-let t_of_sexp : Ppx_sexp_conv_lib.Sexp.t -> t = int_of_sexp
-let sexp_of_t : t -> Ppx_sexp_conv_lib.Sexp.t = sexp_of_int
-let compare : t -> t -> int = compare_int
+let t_of_sexp = (int_of_sexp : Ppx_sexp_conv_lib.Sexp.t -> t)
+let sexp_of_t = (sexp_of_int : t -> Ppx_sexp_conv_lib.Sexp.t)
+let compare = (compare_int : t -> t -> int)
 [@@@end]
 
 type u = int [@@deriving_inline bin_shape { basetype = "foo" }]
