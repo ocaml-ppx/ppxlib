f0b4a13ae0f5a7f2f55f2a66a5f0805ac8de93ba
Xavier Clerc <xclerc@janestreet.com>
2018-03-27 12:38:46 +0100

    Revert the previous tweaks and instead do not import changes related PR#7344.

--- ast/pprintast.ml
+++ ast/pprintast.ml
@@ -256,8 +256,11 @@ and core_type ctxt f x =
           (type_with_label ctxt) (l,ct1) (core_type ctxt) ct2
     | Ptyp_alias (ct, s) ->
         pp f "@[<2>%a@;as@;'%s@]" (core_type1 ctxt) ct s
+    (* Intentionally left out, as part of the fix for PR#7344
+       (commit c15ad73e56c54663c9cb6f7cac4241bb3c4e3cb8)
     | Ptyp_poly ([], ct) ->
         core_type ctxt f ct
+    *)
     | Ptyp_poly (sl, ct) ->
         pp f "@[<2>%a%a@]"
           (fun f l ->
@@ -1190,9 +1193,12 @@ and binding ctxt f {pvb_pat=p; pvb_expr=x; _} =
   if x.pexp_attributes <> []
   then pp f "%a@;=@;%a" (pattern ctxt) p (expression ctxt) x else
   match is_desugared_gadt p x with
+  (* Intentionally left out, as part of the fix for PR#7344
+       (commit c15ad73e56c54663c9cb6f7cac4241bb3c4e3cb8)
   | Some (p, [], ct, e) ->
       pp f "%a@;: %a@;=@;%a"
         (simple_pattern ctxt) p (core_type ctxt) ct (expression ctxt) e
+  *)
   | Some (p, tyvars, ct, e) -> begin
     pp f "%a@;: type@;%a.@;%a@;=@;%a"
     (simple_pattern ctxt) p (list pp_print_string ~sep:"@;")
@@ -1210,16 +1216,9 @@ and binding ctxt f {pvb_pat=p; pvb_expr=x; _} =
               pp f "(%a@;:@;%a)@;=@;%a" (simple_pattern ctxt) p
                 (core_type ctxt) ty (expression ctxt) x
           end
-      | {pexp_desc=Pexp_constraint (x, ty)}, _ ->
-        (* XXX: this case is janestreet only *)
-        begin match ty with
-        | {ptyp_desc=Ptyp_poly _; ptyp_attributes=[]} ->
+      | {pexp_desc=Pexp_constraint (x, ty)}, _ -> (* XXX: this case is janestreet only *)
           pp f "%a@;:@;%a@;=@;%a" (pattern ctxt) p
             (core_type ctxt) ty (expression ctxt) x
-        | _ ->
-          pp f "(%a@;:@;%a)@;=@;%a" (pattern ctxt) p
-            (core_type ctxt) ty (expression ctxt) x
-        end
       | _, {ppat_desc=Ppat_var _; ppat_attributes=[]} ->
           pp f "%a@ %a" (simple_pattern ctxt) p pp_print_pexp_function x
       | _ ->
--- test/driver/omp-integration/test/test.ml
+++ test/driver/omp-integration/test/test.ml
@@ -2,9 +2,9 @@ open Ppx_sexp_conv_lib.Conv
 
 type t = int [@@deriving_inline sexp]
 let _ = fun (_ : t)  -> ()
-let t_of_sexp = (int_of_sexp : Ppx_sexp_conv_lib.Sexp.t -> t)
+let t_of_sexp : Ppx_sexp_conv_lib.Sexp.t -> t = int_of_sexp
 let _ = t_of_sexp
-let sexp_of_t = (sexp_of_int : t -> Ppx_sexp_conv_lib.Sexp.t)
+let sexp_of_t : t -> Ppx_sexp_conv_lib.Sexp.t = sexp_of_int
 let _ = sexp_of_t
 [@@@end]
 
