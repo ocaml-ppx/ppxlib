77507190c4fcd0c9d4c55cad42b13623f9d16226
Rudi Grinberg <rudi.grinberg@gmail.com>
2018-10-08 15:33:08 -0400

    metaocaml should be a reserved name
    Signed-off-by: Rudi Grinberg <rudi.grinberg@gmail.com>

--- src/name.ml
+++ src/name.ml
@@ -142,6 +142,7 @@ module Reserved_namespaces = struct
   let () = reserve "merlin"
   let () = reserve "reason"
   let () = reserve "refmt"
+  let () = reserve "metaocaml"
 
   let is_in_reserved_namespaces name =
     match get_outer_namespace name with
