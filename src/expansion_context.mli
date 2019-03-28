8b5a2011be64c0dec2472633082dae137b02a0dc
Nathan Rebours <nathan.p.rebours@gmail.com>
2019-03-28 18:19:00 +0800

    Move expansion contexts make function to undocumented section
    They are likely to change with the types themselves so they have an
    unstable API and thus users shouldn't rely on those functions.
    
    Signed-off-by: Nathan Rebours <nathan.p.rebours@gmail.com>

--- src/expansion_context.mli
+++ src/expansion_context.mli
@@ -2,9 +2,6 @@ module Extension : sig
   (** Type of expansion contexts for extensions *)
   type t
 
-  (** Build a new expansion context with the given extension point location and code path *)
-  val make : extension_point_loc:Location.t -> code_path:Code_path.t -> t
-
   (** Return the location of the extension point being expanded *)
   val extension_point_loc : t -> Location.t
 
@@ -13,14 +10,17 @@ module Extension : sig
 
   (** Wrap a [fun ~loc ~path] into a [fun ~ctxt] *)
   val with_loc_and_path : (loc:Location.t -> path:string -> 'a) -> (ctxt:t -> 'a)
+
+  (**/**)
+  (** Undocumented section *)
+
+  (** Build a new expansion context with the given extension point location and code path *)
+  val make : extension_point_loc:Location.t -> code_path:Code_path.t -> t
 end
 
 module Deriver : sig
   (** Type of expansion contexts for derivers *)
   type t
-
-  (** Build a new expansion context with the given item location and code path *)
-  val make : derived_item_loc:Location.t -> code_path:Code_path.t -> t
 
   (** Return the location of the item to which the deriver is being applied *)
   val derived_item_loc : t -> Location.t
@@ -30,4 +30,10 @@ module Deriver : sig
 
   (** Wrap a [fun ~loc ~path] into a [fun ~ctxt] *)
   val with_loc_and_path : (loc:Location.t -> path:string -> 'a) -> (ctxt:t -> 'a)
+
+  (**/**)
+  (** Undocumented section *)
+
+  (** Build a new expansion context with the given item location and code path *)
+  val make : derived_item_loc:Location.t -> code_path:Code_path.t -> t
 end
