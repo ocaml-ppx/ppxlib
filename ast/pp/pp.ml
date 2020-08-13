70d85c1a203d245e3d623dde4dfa31d2f24d58f3
Jeremie Dimino <jeremie@dimino.org>
2020-08-13 19:36:02 +0100

    _
    Signed-off-by: Jeremie Dimino <jeremie@dimino.org>

--- ast/pp/pp.ml
+++ ast/pp/pp.ml
@@ -2,21 +2,11 @@ let () =
   match Sys.argv with
   | [|_; ocaml_version_str; fname|] ->
     let ocaml_version =
-      
-      (match Scanf.sscanf ocaml_version_str "%u.%u" (fun a b -> (a, b)) with
-       | (4, 02) -> "402"
-       | (4, 03) -> "403"
-       | (4, 04) -> "404"
-       | (4, 05) -> "405"
-       | (4, 06) -> "406"
-       | (4, 07) -> "407"
-       | (4, 08) -> "408"
-       | (4, 09) -> "409"
-       | (4, 10) -> "410"
-       | (4, 11) -> "411"
-       | _ ->
-         Printf.eprintf "Unknown OCaml version %s\n" ocaml_version_str;
-         exit 1)
+      match Supported_version.of_string ocaml_version_str with
+      | Some v -> string_of_int (Supported_version.to_int v)
+      | None ->
+        Printf.eprintf "Unknown OCaml version %s\n" ocaml_version_str;
+        exit 1
     in
     let is_current =
       (Filename.basename fname = Printf.sprintf "ast_%s.ml" ocaml_version)
