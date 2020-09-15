7871d8ea8368662744503c72f79f7b2044ed91f0
Nathan Rebours <nathan.p.rebours@gmail.com>
2020-09-15 16:42:28 +0200

    Remove support for IF_CURRENT in ppxlib_ast preprocessor
    This was used by OMP but isn't necessary nor makes sense in ppxlib.
    
    Signed-off-by: Nathan Rebours <nathan.p.rebours@gmail.com>

--- ast/pp/pp.ml
+++ ast/pp/pp.ml
@@ -8,12 +8,9 @@ let () =
         Printf.eprintf "Unknown OCaml version %s\n" ocaml_version_str;
         exit 1
     in
-    let is_current =
-      (Filename.basename fname = Printf.sprintf "ast_%s.ml" ocaml_version)
-    in
     let ic = open_in_bin fname in
     Printf.printf "# 1 %S\n" fname;
-    Pp_rewrite.rewrite is_current ocaml_version (Lexing.from_channel ic)
+    Pp_rewrite.rewrite ocaml_version (Lexing.from_channel ic)
   | _ ->
     Printf.eprintf "%s: <ocaml-version> <file-name>\n"
       Sys.executable_name;
