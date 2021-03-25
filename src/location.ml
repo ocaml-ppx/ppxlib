ocaml-ppx/ppxlib

commit 4693339ce7effc8dd5061f05019bf3adc1a2a908

Restore `Location.Error.createf`.

src/location.ml
@@ -50,6 +50,9 @@ module Error = struct
   type t = Helpers.location_error
 
   let make = Helpers.make_error_of_message
+  let createf ~loc fmt =
+    Printf.ksprintf
+      (fun str -> Helpers.make_error_of_message ~loc ~sub:[] str) fmt
 
   let message = Helpers.get_error_message
   let set_message = Helpers.set_error_message

src/location.mli
@@ -40,6 +40,7 @@ module Error : sig
   type t
 
   val make : loc:location -> string -> sub:(location * string) list -> t
+  val createf : loc:location -> ('a, unit, string, t) format4 -> 'a
 
   val message : t -> string
   val set_message : t -> string -> t

commit bb04b52f81c93207b500c1ed058233e259aa673f

Allow "%a" when using Location.Error.createf
This should fix a minor regression introduced in
commit 4693339ce7effc8dd5061f05019bf3adc1a2a908.

Signed-off-by: Marc Lasson <marc.lasson@lexifi.com>

src/location.ml
@@ -81,7 +81,7 @@ module Error = struct
   include Ppxlib_ast.Location_error
 
   let createf ~loc fmt =
-    Printf.ksprintf
+    Format.kasprintf
       (fun str -> make ~loc ~sub:[] str) fmt
 end
 

src/location.mli
@@ -50,7 +50,7 @@ module Error : sig
   type t
 
   val make : loc:location -> string -> sub:(location * string) list -> t
-  val createf : loc:location -> ('a, unit, string, t) format4 -> 'a
+  val createf : loc:location -> ('a, Caml.Format.formatter, unit, t) format4 -> 'a
 
   val message : t -> string
   val set_message : t -> string -> t