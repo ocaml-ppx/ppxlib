8db73f165518de1bb890a0535d5699fd080387e7
Sonja Heinze <sonjaleaheinze@gmail.com>
2021-06-22 11:28:28 +0200

    Move Keyword to Astlib
    For Astlib in ppxlib it's ok to hardcode the list of keywords since the
    keyword table has been stable since 4.04. When upstreaming Astlib to
    the compiler, we can see if we can use the compiler
    `Parsing.Lexer.is_keyword` function, which isn't public at the moment.
    
    Signed-off-by: Sonja Heinze <sonjaleaheinze@gmail.com>

--- astlib/astlib.ml
+++ astlib/astlib.ml
@@ -69,6 +69,7 @@ module Migrate_413_412 = Migrate_413_412
 (* Compiler modules *)
 module Ast_mapper = Ast_mapper
 module Config = Config
+module Keyword = Keyword
 module Location = Location
 module Longident = Longident
 module Parse = Parse
