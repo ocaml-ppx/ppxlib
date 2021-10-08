11257268f67dff2537a9fb636f05f88ef17152b4
Paul-Elliot <peada@free.fr>
2021-10-08 17:58:32 +0200

    linting
    Signed-off-by: Paul-Elliot <peada@free.fr>

--- test/driver/exception_handling/whole_file_exception.ml
+++ test/driver/exception_handling/whole_file_exception.ml
@@ -2,7 +2,8 @@ open Ppxlib
 
 let () =
   Driver.V2.(
-    register_transformation ~impl:(fun _ _ -> failwith "An exception in a whole file transform")
-       "raise_exc")
+    register_transformation
+      ~impl:(fun _ _ -> failwith "An exception in a whole file transform")
+      "raise_exc")
 
 let () = Ppxlib.Driver.standalone ()
