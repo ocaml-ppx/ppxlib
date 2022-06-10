dfec28598a446d2da47c73d9779a14f22e29e20f
Carl Eastlund <ceastlund@janestreet.com>
2022-06-10 11:01:05 -0400

    Test multiple code path accessors.
    Signed-off-by: Carl Eastlund <ceastlund@janestreet.com>

--- test/code_path/test.ml
+++ test/code_path/test.ml
@@ -1,6 +1,16 @@
 #require "base";;
 
+open Base
 open Ppxlib
+
+let sexp_of_code_path code_path =
+  Sexp.message
+    "code_path"
+    [ "main_module_name", sexp_of_string (Code_path.main_module_name code_path)
+    ; "submodule_path", sexp_of_list sexp_of_string (Code_path.submodule_path code_path)
+    ; "value", sexp_of_option sexp_of_string (Code_path.value code_path)
+    ; "fully_qualified_path", sexp_of_string (Code_path.fully_qualified_path code_path)
+    ]
 
 let () =
   Driver.register_transformation "test"
@@ -12,9 +22,10 @@ let () =
            let loc = Expansion_context.Extension.extension_point_loc ctxt in
            let code_path = Expansion_context.Extension.code_path ctxt in
            Ast_builder.Default.estring ~loc
-             (Code_path.fully_qualified_path code_path))
+             (Sexp.to_string (sexp_of_code_path code_path)))
     ]
 [%%expect{|
+val sexp_of_code_path : Code_path.t -> Sexp.t = <fun>
 |}]
 
 let s =
@@ -38,7 +49,8 @@ let s =
   in A.A'.a
 ;;
 [%%expect{|
-val s : string = "Test.s"
+val s : string =
+  "(code_path(main_module_name Test)(submodule_path())(value(s))(fully_qualified_path Test.s))"
 |}]
 
 let module M = struct
@@ -47,5 +59,6 @@ let module M = struct
   in
   M.m
 [%%expect{|
-- : string = "Test"
+- : string =
+"(code_path(main_module_name Test)(submodule_path())(value())(fully_qualified_path Test))"
 |}]
