40deaf5807c6b3891e74b84ef11008dae7b52c48
Carl Eastlund <ceastlund@janestreet.com>
2022-07-14 11:40:56 -0400

    Added lift_map_with_context class to [@@deriving traverse].
    Signed-off-by: Carl Eastlund <ceastlund@janestreet.com>

--- traverse_builtins/ppxlib_traverse_builtins.ml
+++ traverse_builtins/ppxlib_traverse_builtins.ml
@@ -5,6 +5,7 @@ module T = struct
   type ('a, 'acc) fold_map = 'a -> 'acc -> 'a * 'acc
   type ('ctx, 'a) map_with_context = 'ctx -> 'a -> 'a
   type ('a, 'res) lift = 'a -> 'res
+  type ('ctx, 'a, 'res) lift_map_with_context = 'ctx -> 'a -> 'a * 'res
 end
 
 class map =
@@ -159,6 +160,53 @@ class virtual ['res] lift =
         match l with
         | [] -> self#constr "[]" []
         | x :: l -> self#constr "::" [ f x; self#list f l ]
+  end
+
+class virtual ['ctx, 'res] lift_map_with_context =
+  object (self)
+    method virtual other : 'a. 'ctx -> 'a -> 'res
+    method virtual int : ('ctx, int, 'res) T.lift_map_with_context
+    method virtual string : ('ctx, string, 'res) T.lift_map_with_context
+    method virtual bool : ('ctx, bool, 'res) T.lift_map_with_context
+    method virtual char : ('ctx, char, 'res) T.lift_map_with_context
+
+    method virtual array
+        : 'a.
+          'ctx ->
+          ('ctx, 'a, 'res) T.lift_map_with_context ->
+          ('ctx, 'a array, 'res) T.lift_map_with_context
+
+    method virtual float : ('ctx, float, 'res) T.lift_map_with_context
+    method virtual int32 : ('ctx, int32, 'res) T.lift_map_with_context
+    method virtual int64 : ('ctx, int64, 'res) T.lift_map_with_context
+    method virtual nativeint : ('ctx, nativeint, 'res) T.lift_map_with_context
+    method virtual unit : ('ctx, unit, 'res) T.lift_map_with_context
+    method virtual record : 'ctx -> (string * 'res) list -> 'res
+    method virtual constr : 'ctx -> string -> 'res list -> 'res
+    method virtual tuple : 'ctx -> 'res list -> 'res
+
+    method option
+        : 'a.
+          ('ctx, 'a, 'res) T.lift_map_with_context ->
+          ('ctx, 'a option, 'res) T.lift_map_with_context =
+      fun f ctx x ->
+        match x with
+        | None -> (None, self#constr ctx "None" [])
+        | Some x ->
+            let x, res = f ctx x in
+            (Some x, self#constr ctx "Some" [ res ])
+
+    method list
+        : 'a.
+          ('ctx, 'a, 'res) T.lift_map_with_context ->
+          ('ctx, 'a list, 'res) T.lift_map_with_context =
+      fun f ctx l ->
+        match l with
+        | [] -> ([], self#constr ctx "[]" [])
+        | x :: l ->
+            let x, res_head = f ctx x in
+            let l, res_tail = self#list f ctx l in
+            (x :: l, self#constr ctx "::" [ res_head; res_tail ])
   end
 
 class type ['res] std_lifters =
