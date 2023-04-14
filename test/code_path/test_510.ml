50444337f76adb1af04ed39b7a30a8d20bbafdcd
Paul-Elliot <peada@free.fr>
2023-04-14 18:01:21 +0200

    Fix inline tests' dependency on OCaml version
    Done by making the expected output also dependent on the OCaml version
    
    Signed-off-by: Paul-Elliot <peada@free.fr>

---
+++ test/code_path/test_510.ml
@@ -0,0 +1,155 @@
+#require "base";;
+
+open Base
+open Ppxlib
+
+let sexp_of_code_path code_path =
+  Sexp.message
+    "code_path"
+    [ "main_module_name", sexp_of_string (Code_path.main_module_name code_path)
+    ; "submodule_path", sexp_of_list sexp_of_string (Code_path.submodule_path code_path)
+    ; "enclosing_module", sexp_of_string (Code_path.enclosing_module code_path)
+    ; "enclosing_value", sexp_of_option sexp_of_string (Code_path.enclosing_value code_path)
+    ; "value", sexp_of_option sexp_of_string (Code_path.value code_path)
+    ; "fully_qualified_path", sexp_of_string (Code_path.fully_qualified_path code_path)
+    ]
+
+let () =
+  Driver.register_transformation "test"
+    ~extensions:[
+      Extension.V3.declare "code_path"
+        Expression
+        Ast_pattern.(pstr nil)
+        (fun ~ctxt ->
+           let loc = Expansion_context.Extension.extension_point_loc ctxt in
+           let code_path = Expansion_context.Extension.code_path ctxt in
+           Ast_builder.Default.estring ~loc
+             (Sexp.to_string (sexp_of_code_path code_path)))
+    ]
+[%%expect{|
+val sexp_of_code_path : Code_path.t -> Sexp.t = <fun>
+|}]
+
+let s =
+  let module A = struct
+    module A' = struct
+      let a =
+        let module B = struct
+          module B' = struct
+            let b =
+              let module C = struct
+                module C' = struct
+                  let c = [%code_path]
+                end
+              end
+              in C.C'.c
+          end
+        end
+        in B.B'.b
+    end
+  end
+  in A.A'.a
+;;
+[%%expect{|
+val s : string/2 =
+  "(code_path(main_module_name Test_510)(submodule_path())(enclosing_module C')(enclosing_value(c))(value(s))(fully_qualified_path Test_510.s))"
+|}]
+
+let module M = struct
+  let m = [%code_path]
+  end
+  in
+  M.m
+[%%expect{|
+- : string/2 =
+"(code_path(main_module_name Test_510)(submodule_path())(enclosing_module M)(enclosing_value(m))(value())(fully_qualified_path Test_510))"
+|}]
+
+module Outer = struct
+  module Inner = struct
+    let code_path = [%code_path]
+  end
+end
+let _ = Outer.Inner.code_path
+[%%expect{|
+module Outer : sig module Inner : sig val code_path : string/2 end end
+- : string/2 =
+"(code_path(main_module_name Test_510)(submodule_path(Outer Inner))(enclosing_module Inner)(enclosing_value(code_path))(value(code_path))(fully_qualified_path Test_510.Outer.Inner.code_path))"
+|}]
+
+module Functor() = struct
+  let code_path = ref ""
+  module _ = struct
+    let x =
+      let module First_class = struct
+        code_path := [%code_path]
+      end in
+      let module _ = First_class in
+      ()
+    ;;
+
+    ignore x
+  end
+end
+let _ = let module M = Functor() in !M.code_path
+[%%expect{|
+module Functor : functor () -> sig val code_path : string/2 ref end
+Line _:
+Error (warning 73 [generative-application-expects-unit]): A generative functor
+should be applied to '()'; using '(struct end)' is deprecated.
+|}]
+
+module Actual = struct
+  let code_path = [%code_path]
+end [@enter_module Dummy]
+let _ = Actual.code_path
+[%%expect{|
+
+module Actual : sig val code_path : string/2 end
+
+- : string/2 =
+"(code_path(main_module_name Test_510)(submodule_path(Actual Dummy))(enclosing_module Dummy)(enclosing_value(code_path))(value(code_path))(fully_qualified_path Test_510.Actual.Dummy.code_path))"
+|}]
+
+module Ignore_me = struct
+  let code_path = [%code_path]
+end [@@do_not_enter_module]
+let _ = Ignore_me.code_path
+[%%expect{|
+
+module Ignore_me : sig val code_path : string/2 end
+
+- : string/2 =
+"(code_path(main_module_name Test_510)(submodule_path())(enclosing_module Test_510)(enclosing_value(code_path))(value(code_path))(fully_qualified_path Test_510.code_path))"
+|}]
+
+let _ =
+  (let module Ignore_me = struct
+     let code_path = [%code_path]
+   end
+   in
+   Ignore_me.code_path)
+  [@do_not_enter_module]
+[%%expect{|
+
+- : string/2 =
+"(code_path(main_module_name Test_510)(submodule_path())(enclosing_module Test_510)(enclosing_value(code_path))(value())(fully_qualified_path Test_510))"
+|}]
+
+let _ = ([%code_path] [@ppxlib.enter_value dummy])
+[%%expect{|
+
+- : string/2 =
+"(code_path(main_module_name Test_510)(submodule_path())(enclosing_module Test_510)(enclosing_value(dummy))(value(dummy))(fully_qualified_path Test_510.dummy))"
+|}]
+
+let _ =
+  let ignore_me = [%code_path]
+  [@@do_not_enter_value]
+  in
+  ignore_me
+[%%expect{|
+
+- : string/2 =
+"(code_path(main_module_name Test_510)(submodule_path())(enclosing_module Test_510)(enclosing_value())(value())(fully_qualified_path Test_510))"
+|}]
