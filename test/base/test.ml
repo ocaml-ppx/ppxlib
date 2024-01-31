88b3c94a568589ef1faca835ac4a59db622defb4
Nathan Rebours <nathan.p.rebours@gmail.com>
2024-01-31 10:12:47 +0100

    Fix exception raising test cases
    Signed-off-by: Nathan Rebours <nathan.p.rebours@gmail.com>

--- test/base/test.ml
+++ test/base/test.ml
@@ -101,17 +101,17 @@ let _ = convert_longident "Base.( land )"
 
 let _ = convert_longident "A(B)"
 [%%expect{|
-Exception: (Invalid_argument "Ppxlib.Longident.parse: \"A(B)\"")
+Exception: Invalid_argument "Ppxlib.Longident.parse: \"A(B)\"".
 |}]
 
 let _ = convert_longident "A.B(C)"
 [%%expect{|
-Exception: (Invalid_argument "Ppxlib.Longident.parse: \"A.B(C)\"")
+Exception: Invalid_argument "Ppxlib.Longident.parse: \"A.B(C)\"".
 |}]
 
 let _ = convert_longident ")"
 [%%expect{|
-Exception: (Invalid_argument "Ppxlib.Longident.parse: \")\"")
+Exception: Invalid_argument "Ppxlib.Longident.parse: \")\"".
 |}]
 
 (* FIXME this is a bug *)
