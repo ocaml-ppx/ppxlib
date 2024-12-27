36a0714a49f1a52ae1c37e63492032cca6936be1
Pedro B S Lisboa <pedrobslisboa@gmail.com>
2024-12-27 17:31:12 -0300

    Add ast sample
    Signed-off-by: Pedro B S Lisboa <pedrobslisboa@gmail.com>

---
+++ examples/1-AST/ast_explorer.ml
@@ -0,0 +1,8 @@
+(*
+    Add any code you want to explore here
+    and run `ppxlib-pp-ast ./examples/1-AST/ast_explorer.ml` on the repo root to see the AST
+*)
+
+(* Sample code, edit as you wish *)
+let name = "john doe"
+let age = 99
