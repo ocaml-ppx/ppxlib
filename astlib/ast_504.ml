9e231155dfbc509953bef8ad5d163684fcb7fcf9
Patrick Ferris <patrick@sirref.org>
2025-04-19 11:44:32 +0100

    Bump the magic numbers for AST 504
    Signed-off-by: Patrick Ferris <patrick@sirref.org>

--- astlib/ast_504.ml
+++ astlib/ast_504.ml
@@ -1173,6 +1173,6 @@ module Parsetree = struct
 end
 
 module Config = struct
-  let ast_impl_magic_number = "Caml1999M035"
-  let ast_intf_magic_number = "Caml1999N035"
+  let ast_impl_magic_number = "Caml1999M036"
+  let ast_intf_magic_number = "Caml1999N036"
 end
