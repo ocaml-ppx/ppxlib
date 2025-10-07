18bd96f1e18e00628c908a473e4acd412beae11d
Nathan Rebours <nathan.rebours@ocamlpro.com>
2025-10-07 17:03:19 +0200

    Add test for pprintast handling of mod
    Signed-off-by: Nathan Rebours <nathan.rebours@ocamlpro.com>

---
+++ test/pprintast/raw_identifiers/test.ml
@@ -0,0 +1,42 @@
+open Ppxlib
+
+let identifier = Longident.Lident "mod"
+;;
+[%%ignore]
+
+Format.asprintf "%a" Pprintast.longident identifier
+;;
+[%%expect{|
+- : string = "\\#mod"
+|}]
+
+module Build = Ast_builder.Make(struct let loc = Location.none end)
+
+(* 10 mod 3 *)
+let expr =
+  let open Build in
+  eapply (pexp_ident (Located.mk identifier)) [(eint 10); (eint 3)]
+;;
+[%%ignore]
+
+Format.asprintf "%a" Pprintast.expression expr
+;;
+[%%expect{|
+- : string = "10 mod 3"
+|}]
+
+(* [let f = (mod) *)
+let stri =
+  let open Build in
+  pstr_value Nonrecursive
+    [ value_binding
+        ~pat:(pvar "f")
+        ~expr:(pexp_ident (Located.mk identifier))
+    ]
+;;
+[%%ignore]
+
+Format.asprintf "%a" Pprintast.structure_item stri
+[%%expect{|
+- : string = "let f = \\#mod"
+|}]
